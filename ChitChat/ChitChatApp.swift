fd58783882a5895f97f67899e559792317a29eef
Lau <lausandoval@uc.cl>
2022-06-10 15:31:26 -0400

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Lau on 10-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
