610bcca5a09e35ae8b5f354abe67b3c4ac2c55c1
Lau <lausandoval@uc.cl>
2022-06-10 15:43:57 -0400

    chore: basic app UI

--- ChitChat/ContentView.swift
+++ ChitChat/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var phoneNumber: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    HStack {
+                        Button(action: {
+                            print("Choose App")
+                        }) {
+                            HStack {
+                                Image("WhatsApp")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 29, height: 29, alignment: .center)
+                                    .clipShape(RoundedRectangle(cornerRadius: 7.0, style: .continuous))
+                                Image(systemName: "chevron.down")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 18, height: 18)
+                            }
+                        }
+                        
+                        TextField("Enter or paste a phone number", text: $phoneNumber)
+                    }
+                }
+                Section {
+                    
+                } header: {
+                    Text("Recents")
+                }
+            }
+            .navigationTitle("Chit Chat")
+        }
     }
 }
 
