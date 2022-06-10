34d5a38bb6d24fbc8434e38658e12e99902f346c
Lau <lausandoval@uc.cl>
2022-06-10 16:02:25 -0400

    chore: add "Paste" or "Start Chat" button

--- ChitChat/ContentView.swift
+++ ChitChat/ContentView.swift
@@ -11,38 +11,65 @@ struct ContentView: View {
     @State private var phoneNumber: String = ""
     
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    HStack(spacing: 14.0) {
-                        Button(action: {
-                            print("Choose App")
-                        }) {
-                            HStack(spacing: 6.0) {
-                                Image("WhatsApp")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 29, height: 29, alignment: .center)
-                                    .clipShape(RoundedRectangle(cornerRadius: 7.0, style: .continuous))
-                                Image(systemName: "chevron.down")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 15, height: 15)
+        ZStack {
+            NavigationView {
+                List {
+                    Section {
+                        HStack(spacing: 14.0) {
+                            Button(action: {
+                                print("Choose App")
+                            }) {
+                                HStack(spacing: 6.0) {
+                                    Image("WhatsApp")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 29, height: 29, alignment: .center)
+                                        .clipShape(RoundedRectangle(cornerRadius: 7.0, style: .continuous))
+                                    Image(systemName: "chevron.down")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 15, height: 15)
+                                }
                             }
+                            .buttonStyle(PlainButtonStyle())
+                            .foregroundColor(.accentColor)
+                            
+                            TextField("Enter or paste a phone number", text: $phoneNumber)
+                                .keyboardType(.phonePad)
+                                .textContentType(.telephoneNumber)
                         }
-                        .buttonStyle(PlainButtonStyle())
-                        .foregroundColor(.accentColor)
-                        
-                        TextField("Enter or paste a phone number", text: $phoneNumber)
+                    }
+                    Section {
+                        Text("No recents.")
+                    } header: {
+                        Text("Recents")
                     }
                 }
-                Section {
-                    
-                } header: {
-                    Text("Recents")
-                }
+                .navigationTitle("Chit Chat")
             }
-            .navigationTitle("Chit Chat")
+            
+            VStack {
+                Spacer()
+                
+                VStack {
+                    Button(action: {
+                        print("Paste or Start Chat")
+                    }) {
+                        HStack {
+                            Spacer()
+                            Text("Paste")
+                            Spacer()
+                        }
+                    }
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.accentColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                }
+                .padding()
+                .background(.thinMaterial)
+            }
         }
     }
 }
